94d41908c2d538b490437e6d20fb5ab9b2197c13
WangRunzhe <E0005433@u.nus.edu>
2015-08-14 14:04:39 +0800

    WangRunzhe A0141466H

--- CSExercises/Ex01-06/Ex02.cs
+++ CSExercises/Ex01-06/Ex02.cs
@@ -10,7 +10,11 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            int x;
+            System.Console.WriteLine("Please input a integer.");
+            x = System.Convert.ToInt32(Console.ReadLine());
+            x = x * x;
+            System.Console.WriteLine("x^2={0}", x);
         }
     }
 }
